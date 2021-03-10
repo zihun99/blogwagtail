07d3d9d8578f21c5fcc68edb6b3d7a51ff3f1dd7
Debian <bitnami@ip-172-26-5-153.ap-northeast-2.compute.internal>
2021-03-10 06:42:56 +0000

    ok

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,8 +1,5 @@
 import os
-import sys
-sys.path.append('/home/bitnami/venvs/blog/mysite/')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/home/bitnami/venvs/blog/mysite/egg_cache")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tutorial.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
